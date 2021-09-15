220711a5669a7693dc5c3db677d2ddef0c40412e
lucas1195 <lucaslaurindo1195@gmail.com>
2021-09-15 18:28:49 -0300

    Atividade 4 - Backend

---
+++ src/test/java/com/WebApplication/crm/WabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebApplication.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
